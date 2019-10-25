ccc36d6a55999beee566ecd21e6eefab113e98b9
thejadedfire <thejadedfire@gmail.com>
2019-10-25 19:34:02 -0400

    Update TestBot.js

--- TestBot.js
+++ TestBot.js
@@ -10,25 +10,20 @@ client.on('ready', () => {
 
 });
 
+ 
 
+client.on('message', message => {
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-  else if (msg.content === 'Henry Jekyll'){
-    msg.reply("At your service, " + msg.author);
-  } else if (msg.content === "Gucci"){
-      let chan = msg.channel;
-      const attachment = new Attachment('https://i.imgur.com/Zbqj2lW.jpg');
-      //var emoji = msg.author.guild.emojis.find(em => emoji.name === "shame");
-      //chan.send(emoji);
-      chan.send("Fashionable.", attachment);
-  }
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
 });
 
  
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
