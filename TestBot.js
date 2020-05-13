e0d52c30e2174ca412fcb91f00753c9e6a79da69
thejadedfire <thejadedfire@gmail.com>
2020-05-12 20:52:29 -0400

    Update TestBot.js

--- TestBot.js
+++ TestBot.js
@@ -11,6 +11,12 @@ client.on('ready', () => {
 });
 
  
+client.on('ready', () => {
+  console.log('I am ready!');	  console.log(`Logged in as ${client.user.tag}!`);
+  const channel = member.guild.channels.find(ch => ch.name === 'servant-quarters');
+  const date = Date.now();
+  channel.send("I came online at " + date.toDateString() + ", " + date.toTimeString());
+});
 
 client.on('message', message => {
 
